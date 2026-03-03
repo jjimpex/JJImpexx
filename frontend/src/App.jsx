708ab27c2380c2f5120f6f21cc301d646720a801
jjass-kaur <jaskirat22.kaur@gmail.com>
2026-03-04 00:54:01 +0530

    fixed build error 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import MainLayout from "./components/layout/MainLayout";
 import Home from "./pages/Home";
 import BrandProducts from "./pages/BrandProducts";
-import CategoryProducts from "./pages/categoryProducts";
+import CategoryProducts from "../src/pages/CategoryProducts";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 
