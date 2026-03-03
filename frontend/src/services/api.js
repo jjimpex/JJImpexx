cc43ccc995225a3b7e8e4434efa1420080abb7d8
jjass-kaur <jaskirat22.kaur@gmail.com>
2026-03-04 00:59:42 +0530

    fixed api error

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,15 +1,8 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,
-// });
-
-// export default api;
-
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default api;
